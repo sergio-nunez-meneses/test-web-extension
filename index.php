db593cba0e2e20c463775d11b954dd06e767d408
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-05-15 18:43:08 +0200

    Add base html and css

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+	<div class="divContainer">
+		<div id="cookieDiv" class="removeDiv">
+			<div id="cookieText">
+				<p>Accept cookies to access</p>
+			</div>
+			<div id="cookieBtn">
+				<button>Accept</button>
+			</div>
+		</div>
+		<div id="loginDiv" class="removeDiv">
+			<div id="loginText">
+				<p>Login to to see more</p>
+			</div>
+			<div id="loginBtn">
+				<button>Login</button>
+			</div>
+		</div>
+	</div>
+	<div class="divContainer">
+		<button id="myButton">Activate Extension</button>
+	</div>
+
 	<script src="script.js"></script>
 </body>
 </html>
